15d3212137859347ec014302622780f6e05191ad
astrapi69 <asterios.raptis@gmx.net>
2022-05-17 10:43:26 +0200

    initial version

---
+++ src/main/java/io/github/astrapi69/testing/Brand.java
@@ -0,0 +1,6 @@
+package io.github.astrapi69.testing;
+
+public enum Brand
+{
+	FERRARI
+}
---
+++ src/test/java/io/github/astrapi69/testing/LibraryTests.java
@@ -0,0 +1,13 @@
+package io.github.astrapi69.testing;
+
+import org.junit.jupiter.api.Test;
+
+public class LibraryTests
+{
+
+	@Test public void testSomeLibraryMethod()
+	{
+		System.out.println("here comes your tests");
+	}
+
+}
